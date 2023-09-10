30dc2da251533f6425eba84fc97fd068840ae4bd
Bruno Santimaria <brun0.23@outlook.com>
2023-09-10 19:24:24 -0300

    agregado router

---
+++ src/heroes/routes/HeroesRoutes.jsx
@@ -0,0 +1,20 @@
+import { Navbar } from "../../ui/components/NavBar";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { MarvelPages, DcPage, SearchPage, HeroPage } from "../pages";
+
+export const HeroesRoutes = () => {
+  return (
+    <>
+      <Navbar />
+      <div className='container mt-4'>
+        <Routes>
+          <Route path='marvel' element={<MarvelPages />} />
+          <Route path='dc' element={<DcPage />} />
+          <Route path='*' element={<Navigate to={"/marvel"} />} />
+          <Route path='search' element={<SearchPage />} />
+          <Route path='hero' element={<HeroPage />} />
+        </Routes>
+      </div>
+    </>
+  );
+};
